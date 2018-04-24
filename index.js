f6b915608b734232f71e8f6e50768212e4dd5f6c
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-24 20:53:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ class Route {
   
   estimatedTime(peak) {
     if (peak) 
-      return (blocksTravelled() / 2);
+      return (this.blocksTravelled() / 2);
     else 
-      return (blocksTravelled() /3);
+      return (this.blocksTravelled() /3);
   }
 }
